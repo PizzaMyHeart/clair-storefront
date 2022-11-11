e12d011342f8b2161e0bec0fae88fc0400821dc9
PizzaMyHeart <ian_tan@hotmail.com>
2022-11-11 20:29:49 +0000

    overflow y scroll

--- src/components/header/region-popover.jsx
+++ src/components/header/region-popover.jsx
@@ -34,7 +34,7 @@ const RegionPopover = () => {
       </div>
 
       <PopoverTransition>
-        <Menu.Items className="origin-top-right absolute right-0 mt-2 w-56 px-6 py-2 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none overflow-y-auto">
+        <Menu.Items className="origin-top-right absolute right-0 mt-2 w-56 px-6 py-2 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none overflow-y-scroll">
           {regions?.map(region => {
             return region.countries.map((country, i) => {
               return (
