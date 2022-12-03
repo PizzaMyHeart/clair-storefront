31c627dc3445775ab1c1b05f9cc4893bab1c6fbb
PizzaMyHeart <ian_tan@hotmail.com>
2022-12-03 15:00:50 +0000

    updated banner text

--- src/components/header/banner.jsx
+++ src/components/header/banner.jsx
@@ -22,7 +22,7 @@ const Banner = () => {
         "bg-ui-dark min-h-10 py-[10px] px-[20px] flex items-center justify-between text-sm font-medium text-white px-4 sm:px-6 lg:px-8 text-center"
       )}
     >
-      <p className="basis-full">Free shipping on all orders in Malaysia | Worldwide shipping available from 14 Feb 2023</p>
+      <p className="basis-full">Free shipping on all orders in Malaysia | Worldwide shipping available from 13 Feb 2023 | Get 10% off everything with code: CLAIRLONDON</p>
       <button onClick={() => hideBanner()}>&times;</button>
     </div>
   )
