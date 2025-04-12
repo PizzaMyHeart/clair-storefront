f97caa7e6c41c3bd5527adfec53d2fe351bec3f2
PizzaMyHeart <ian_tan@hotmail.com>
2025-04-12 16:25:36 +0100

    Revert "changed home page bottom banner to etsy"
    This reverts commit e633d128ad82032e4df1af1cec785c0399bf8985.

--- src/pages/index.js
+++ src/pages/index.js
@@ -74,8 +74,8 @@ const IndexPage = ({ data }) => {
           </Grid>
           <div className="mt-12 mb-12">
           <StaticImage
-              src="../images/etsy_banner.jpg"
-              alt="A collage of Clair jewellery"
+              src="../images/hope_header.png"
+              alt="A necklace hanging from a Christmas tree"
               placeholder="tracedSVG"
               className="w-full h-auto"
             />
